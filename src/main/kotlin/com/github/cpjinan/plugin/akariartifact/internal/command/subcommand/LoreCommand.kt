6965a66210e58f25252f23d2f7aa06d21210937e
季楠 <2759278070@qq.com>
2024-08-18 19:58:40 +0800

    [1.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akariartifact/internal/command/subcommand/LoreCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariartifact/internal/command/subcommand/LoreCommand.kt
@@ -11,6 +11,8 @@ import taboolib.module.lang.sendLang
 import taboolib.platform.util.modifyLore
 
 object LoreCommand {
+    val clipboard: HashMap<Player, String> = hashMapOf()
+
     val lore = subCommand {
         literal("add") {
             dynamic("lore") {
@@ -39,9 +41,8 @@ object LoreCommand {
                         item?.modifyLore {
                             add(context["lore"].colored())
                         }
+                        if (!silent) sender.sendLang("Add-Lore", context["lore"].colored())
                     }
-
-                    if (!silent) sender.sendLang("Add-Lore", context["lore"].colored())
                 }
             }
         }
@@ -73,9 +74,8 @@ object LoreCommand {
                         item?.modifyLore {
                             removeAt(context.int("line") - 1)
                         }
+                        if (!silent) sender.sendLang("Remove-Lore", context.int("line"))
                     }
-
-                    if (!silent) sender.sendLang("Remove-Lore", context.int("line"))
                 }
             }
         }
@@ -107,9 +107,8 @@ object LoreCommand {
                         item?.modifyLore {
                             set(context.int("line") - 1, context["lore"].colored())
                         }
+                        if (!silent) sender.sendLang("Set-Lore", context.int("line"), context["lore"].colored())
                     }
-
-                    if (!silent) sender.sendLang("Set-Lore", context.int("line"), context["lore"].colored())
                 }
             }
         }
@@ -122,7 +121,12 @@ object LoreCommand {
                         item?.modifyLore {
                             add(context.int("line") - 1, context["lore"].colored())
                         }
-                        sender.sendLang("Insert-Lore", context.int("line"), context.int("line") + 1, context["lore"].colored())
+                        sender.sendLang(
+                            "Insert-Lore",
+                            context.int("line"),
+                            context.int("line") + 1,
+                            context["lore"].colored()
+                        )
                     }
                 }
             }.dynamic("options") {
@@ -140,18 +144,226 @@ object LoreCommand {
                         val item = it?.inventory?.itemInMainHand
                         item?.modifyLore {
                             add(context.int("line") - 1, context["lore"].colored())
-                            colored()
+                        }
+                        if (!silent) sender.sendLang(
+                            "Insert-Lore",
+                            context.int("line"),
+                            context.int("line") + 1,
+                            context["lore"].colored()
+                        )
+                    }
+                }
+            }
+        }
+
+        literal("clear") {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                sender.castSafely<Player>().let {
+                    val item = it?.inventory?.itemInMainHand
+                    item?.modifyLore {
+                        clear()
+                    }
+                    sender.sendLang("Clear-Lore")
+                }
+            }
+        }.dynamic("options") {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                val args = CommandUtil.parseOptions(content.split(" "))
+                var silent = false
+
+                for ((k, _) in args) {
+                    when (k.lowercase()) {
+                        "silent" -> silent = true
+                    }
+                }
+
+                sender.castSafely<Player>().let {
+                    val item = it?.inventory?.itemInMainHand
+                    item?.modifyLore {
+                        clear()
+                    }
+                    if (!silent) sender.sendLang("Clear-Lore")
+                }
+            }
+        }
+
+        literal("clone") {
+            int("lineA").int("lineB") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            set(context.int("lineB") - 1, get(context.int("lineA") - 1))
+                        }
+                        sender.sendLang("Clone-Lore", context.int("lineA"), context.int("lineB"))
+                    }
+                }
+            }.dynamic("options") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                    val args = CommandUtil.parseOptions(content.split(" "))
+                    var silent = false
+
+                    for ((k, _) in args) {
+                        when (k.lowercase()) {
+                            "silent" -> silent = true
                         }
                     }
 
-                    if (!silent) sender.sendLang(
-                        "Insert-Lore",
-                        context.int("line"),
-                        context.int("line") + 1,
-                        context["lore"].colored()
-                    )
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            set(context.int("lineB") - 1, get(context.int("lineA") - 1))
+                        }
+                        if (!silent) sender.sendLang("Clone-Lore", context.int("lineA"), context.int("lineB"))
+                    }
                 }
             }
+        }
+
+        literal("copy") {
+            int("line") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            val value = get(context.int("line") - 1)
+                            clipboard[it] = value
+                            sender.sendLang("Copy-Lore", context.int("line"), value)
+                        }
+                    }
+                }
+            }.dynamic("options") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                    val args = CommandUtil.parseOptions(content.split(" "))
+                    var silent = false
+
+                    for ((k, _) in args) {
+                        when (k.lowercase()) {
+                            "silent" -> silent = true
+                        }
+                    }
+
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            val value = get(context.int("line") - 1)
+                            clipboard[it] = value
+                            if (!silent) sender.sendLang("Copy-Lore", context.int("line"), value)
+                        }
+                    }
+
+                }
+            }
+        }
+
+        literal("paste") {
+            int("line") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            clipboard[it]?.colored()?.let { value ->
+                                if (clipboard.isNotEmpty()) {
+                                    set(context.int("line"), value)
+                                    sender.sendLang("Paste-Lore", context.int("line"), value)
+                                } else sender.sendLang("Clipboard-Empty")
+                            }
+                        }
+                    }
+                }
+            }.dynamic("options") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                    val args = CommandUtil.parseOptions(content.split(" "))
+                    var silent = false
+
+                    for ((k, _) in args) {
+                        when (k.lowercase()) {
+                            "silent" -> silent = true
+                        }
+                    }
+
+                    sender.castSafely<Player>().let {
+                        val item = it?.inventory?.itemInMainHand
+                        item?.modifyLore {
+                            clipboard[it]?.colored()?.let { value ->
+                                if (clipboard.isNotEmpty()) {
+                                    set(context.int("line"), value)
+                                    if (!silent) sender.sendLang("Paste-Lore", context.int("line"), value)
+                                } else if (!silent) sender.sendLang("Clipboard-Empty")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        literal("clipboard") {
+
+            literal("check") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, _: CommandContext<ProxyCommandSender>, _: String ->
+                    sender.castSafely<Player>().let {
+                        sender.sendLang(
+                            "Clipboard-Check",
+                            clipboard[it] ?: sender.sendLang("Clipboard-Empty")
+                        )
+                    }
+                }
+            }
+
+            literal("set") {
+                dynamic("value") {
+                    execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                        sender.castSafely<Player>().let {
+                            clipboard[it!!] = context["value"]
+                        }
+                        sender.sendLang("Clipboard-Set", context["value"])
+                    }
+                }.dynamic("options") {
+                    execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                        val args = CommandUtil.parseOptions(content.split(" "))
+                        var silent = false
+
+                        for ((k, _) in args) {
+                            when (k.lowercase()) {
+                                "silent" -> silent = true
+                            }
+                        }
+
+                        sender.castSafely<Player>().let {
+                            clipboard[it!!] = context["value"]
+                        }
+
+                        if (!silent) sender.sendLang("Clipboard-Set", context["value"])
+                    }
+                }
+            }
+
+            literal("clear") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, _: CommandContext<ProxyCommandSender>, _: String ->
+                    sender.castSafely<Player>().let {
+                        clipboard[it!!] = ""
+                        sender.sendLang("Clipboard-Clear")
+                    }
+                }
+            }.dynamic("options") {
+                execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                    val args = CommandUtil.parseOptions(content.split(" "))
+                    var silent = false
+
+                    for ((k, _) in args) {
+                        when (k.lowercase()) {
+                            "silent" -> silent = true
+                        }
+                    }
+
+                    sender.castSafely<Player>().let {
+                        clipboard[it!!] = ""
+                    }
+
+                    if (!silent) sender.sendLang("Clipboard-Clear")
+                }
+            }
+
         }
     }
 }
