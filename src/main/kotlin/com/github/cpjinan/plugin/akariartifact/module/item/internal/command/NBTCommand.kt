ad99c43b9e7fbf044b05e2f94849d5ab918e8099
季楠 <2759278070@qq.com>
2024-09-10 21:22:49 +0800

    [1.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/item/internal/command/NBTCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/item/internal/command/NBTCommand.kt
@@ -1,5 +1,6 @@
 package com.github.cpjinan.plugin.akariartifact.module.item.internal.command
 
+import com.github.cpjinan.plugin.akariartifact.core.utils.CommandUtil
 import com.github.cpjinan.plugin.akariartifact.module.item.ModuleItem
 import org.bukkit.entity.Player
 import taboolib.common.platform.ProxyCommandSender
@@ -8,10 +9,7 @@ import taboolib.common.platform.command.subCommand
 import taboolib.expansion.createHelper
 import taboolib.module.chat.colored
 import taboolib.module.lang.sendLang
-import taboolib.module.nms.ItemTag
-import taboolib.module.nms.ItemTagList
-import taboolib.module.nms.ItemTagType
-import taboolib.module.nms.getItemTag
+import taboolib.module.nms.*
 import taboolib.platform.util.isAir
 
 object NBTCommand {
@@ -21,67 +19,107 @@ object NBTCommand {
 
         literal("check") {
             execute<ProxyCommandSender> { sender: ProxyCommandSender, _: CommandContext<ProxyCommandSender>, _: String ->
-                try {
-                    sender.castSafely<Player>().let {
-                        val item = it?.inventory?.itemInMainHand
-                        if (item.isAir()) {
-                            sender.sendLang("Air-In-Hand")
-                            return@execute
-                        }
+                sender.castSafely<Player>().let {
+                    val item = it?.inventory?.itemInMainHand
+                    if (item.isAir()) {
+                        sender.sendLang("Air-In-Hand")
+                        return@execute
+                    }
 
-                        val itemTag = item.getItemTag()
-                        sender.sendLang("NBT-Check")
-                        fun runAny(player: Player, data: ItemTag, indent: String) {
-                            for ((key, value) in data) {
-                                when (value) {
-                                    is ItemTag -> {
-                                        player.sendMessage("§7$indent$key:")
-                                        runAny(player, value, "$indent  ")
+                    val itemTag = item.getItemTag()
+                    sender.sendLang("NBT-Check")
+                    fun runAny(player: Player, data: ItemTag, indent: String) {
+                        for ((key, value) in data) {
+                            when (value) {
+                                is ItemTag -> {
+                                    player.sendMessage("§7$indent$key:")
+                                    runAny(player, value, "$indent  ")
+                                }
+
+                                is ItemTagList -> {
+                                    player.sendMessage("&7$indent$key&8:".colored())
+                                    value.forEach { v ->
+                                        when (v.type) {
+                                            ItemTagType.COMPOUND -> {
+                                                runAny(player, v.asCompound(), "$indent  ")
+                                            }
+
+                                            else -> {
+                                                player.sendMessage("$indent  &f- &f$v".colored())
+                                            }
+                                        }
+
                                     }
+                                }
 
-                                    is ItemTagList -> {
-                                        player.sendMessage("&7$indent$key&8:".colored())
-                                        value.forEach { v ->
-                                            when (v.type) {
-                                                ItemTagType.COMPOUND -> {
-                                                    runAny(player, v.asCompound(), "$indent  ")
-                                                }
-
-                                                else -> {
+                                is List<*> -> {
+                                    player.sendMessage("§7$indent$key:")
+                                    value.forEach { u ->
+                                        when (u) {
+                                            is ItemTagList -> {
+                                                player.sendMessage("&7$indent$key&8:".colored())
+                                                u.forEach { v ->
                                                     player.sendMessage("$indent  &f- &f$v".colored())
                                                 }
                                             }
 
+                                            else -> player.sendMessage("$indent  &f- &f$value".colored())
                                         }
                                     }
+                                }
 
-                                    is List<*> -> {
-                                        player.sendMessage("§7$indent$key:")
-                                        value.forEach { u ->
-                                            when (u) {
-                                                is ItemTagList -> {
-                                                    player.sendMessage("&7$indent$key&8:".colored())
-                                                    u.forEach { v ->
-                                                        player.sendMessage("$indent  &f- &f$v".colored())
-                                                    }
-                                                }
-
-                                                else -> player.sendMessage("$indent  &f- &f$value".colored())
-                                            }
-                                        }
-                                    }
-
-                                    else -> {
-                                        player.sendMessage("$indent&7$key&8: &f$value".colored())
-                                    }
+                                else -> {
+                                    player.sendMessage("$indent&7$key&8: &f$value".colored())
                                 }
                             }
                         }
-                        runAny(sender.cast(), itemTag, "  ")
                     }
-                } catch (error: IndexOutOfBoundsException) {
-                    sender.sendLang("Index-Out-Of-Bounds")
+                    runAny(sender.cast(), itemTag, "  ")
                 }
+            }
+        }
+
+        literal("set").dynamic("key").dynamic("value") {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                val item = sender.castSafely<Player>()?.inventory?.itemInMainHand
+                if (item.isAir()) {
+                    sender.sendLang("Air-In-Hand")
+                    return@execute
+                }
+
+                item.itemTagReader {
+                    set(context["key"], context["value"])
+                    write(item)
+                }
+
+                sender.castSafely<Player>()?.inventory?.itemInMainHand = item
+                sender.sendLang("NBT-Set", context["key"], context["value"])
+            }
+        }.dynamic("options") {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                val value = context["value"] + CommandUtil.parseContentAfterSpace(content.split(" "))
+                val args = CommandUtil.parseOptions(content.split(" "))
+                var silent = false
+
+                for ((k, _) in args) {
+                    when (k.lowercase()) {
+                        "silent" -> silent = true
+                    }
+                }
+
+                val item = sender.castSafely<Player>()?.inventory?.itemInMainHand
+                if (item.isAir()) {
+                    sender.sendLang("Air-In-Hand")
+                    return@execute
+                }
+
+                item.itemTagReader {
+                    set(context["key"], value)
+                    write(item)
+                }
+
+                sender.castSafely<Player>()?.inventory?.itemInMainHand = item
+                if (!silent) sender.sendLang("NBT-Set", context["key"], value)
             }
         }
     }
