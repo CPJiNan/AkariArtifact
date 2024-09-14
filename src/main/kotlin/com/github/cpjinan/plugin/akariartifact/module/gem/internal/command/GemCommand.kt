0f90621e09acd337af4e67d65d5c1a865d04f76a
季楠 <2759278070@qq.com>
2024-09-14 11:56:22 +0800

    [1.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/gem/internal/command/GemCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/gem/internal/command/GemCommand.kt
@@ -1,9 +1,23 @@
 package com.github.cpjinan.plugin.akariartifact.module.gem.internal.command
 
+import com.github.cpjinan.plugin.akariartifact.module.gem.api.GemAPI
+import org.bukkit.entity.Player
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.CommandContext
 import taboolib.common.platform.command.subCommand
 
 object GemCommand {
     val gem = subCommand {
-
+        literal("socket").dynamic("gem") {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                val item = sender.cast<Player>().inventory.itemInMainHand
+                GemAPI.socketGem(sender.cast(), item, context["gem"])
+            }
+        }.dynamic("options", optional = true) {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, content: String ->
+                val item = sender.cast<Player>().inventory.itemInMainHand
+                GemAPI.socketGem(sender.cast(), item, context["gem"])
+            }
+        }
     }
 }
