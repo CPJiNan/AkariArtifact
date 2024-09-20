4655d086e8c8419c64e22d7ca1665a90b793fabf
季楠 <2759278070@qq.com>
2024-09-20 13:19:59 +0800

    [1.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/skill/internal/listener/SkillListener.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariartifact/module/skill/internal/listener/SkillListener.kt
@@ -3,6 +3,7 @@ package com.github.cpjinan.plugin.akariartifact.module.skill.internal.listener
 import com.github.cpjinan.plugin.akariartifact.core.common.script.kether.Kether.evalKether
 import com.github.cpjinan.plugin.akariartifact.module.skill.ModuleSkill
 import com.github.cpjinan.plugin.akariartifact.module.skill.api.SkillAPI
+import com.github.cpjinan.plugin.akariartifact.module.skill.common.SkillCooldown
 import org.bukkit.event.player.PlayerInteractEvent
 import taboolib.common.platform.event.SubscribeEvent
 import taboolib.platform.util.sendLang
@@ -18,8 +19,13 @@ object SkillListener {
         val skillName = SkillAPI.getSkillNames().firstOrNull { SkillAPI.isBindSkill(item, it) } ?: return
         val skillSection = SkillAPI.getSkillSections()[skillName] ?: return
 
-        if (!skillSection.getStringList("Condition").all { it.evalKether(player).toString().toBoolean() }) {
-            player.sendLang("Skill-Condition-Not-Met")
-        }
+        if (!skillSection.getStringList("Condition")
+                .all { it.evalKether(player).toString().toBoolean() }
+        ) player.sendLang("Skill-Condition-Not-Met")
+
+        if (!SkillCooldown.isCooldownFinish("${player.name}.$skillName")) player.sendLang(
+            "Skill-Cooldown",
+            SkillCooldown.getCooldown("${player.name}.$skillName")
+        )
     }
 }
